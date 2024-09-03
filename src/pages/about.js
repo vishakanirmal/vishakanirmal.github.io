fcf122accd8cb670c0df5f0ca418240f3eb4a4be
vishakanirmal <heyvishaka@gmail.com>
2024-09-03 11:07:33 -0400

    small changes on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,6 +3,12 @@ import React from 'react';
 import Layout from '../components/layout';
 import '../styles/global.css';
 import image from '/src/images/profile.jpg';
+import uw from '/src/images/logos/uw.png';
+import brown from '/src/images/logos/brown.png';
+import risd from '/src/images/logos/risd.png';
+import lucid from '/src/images/logos/lucid.png';
+import microsoft from '/src/images/logos/microsoft.jpg';
+import metro from '/src/images/logos/metro.png';
 import TypingAnimationAbout from '../components/TypingAnimationAbout';
 
 const AboutPage = () => {
@@ -22,11 +28,29 @@ const AboutPage = () => {
             </div>
             <div>
               <p>
-                With my undergrad at the University of Washington, I got to build upon my observation skills. I graduated with a B.S. in Human Centered Design & Engineering, with concentration tracks in Human-Computer Interaction, and 3D animation. Throughout the past couple years, I've gotten the opportunity to work with teams such as Microsoft Dynamics, and Lucid Software. With 3.5 years of industry experience in my toolkit, I'm now excited to pursue my Masters degree. I'll be spending my time at both RISD and Brown, honing in my digital+physical prototyping skills with a degree in Design Engineering.
+               I am a human-centered designer currently focused on crafting virtual collaboration tools. My academic journey in Human-Centered Design & Engineering from the University of Washington, combined with my ongoing studies in Design Engineering at Brown University and the Rhode Island School of Design, equips me with a deep understanding of user needs and effective design.
               </p>
               <p>
-                Throughout my design journey, I've strived to be an advocate for user needs, beautiful design, and functional experiences. And when I'm not engrossed in a design problem, you can often find me walking my dog Olive through a new park.
+              With three years of experience as a UX Designer at Lucid Software and Microsoft Dynamics 365, I’ve had the opportunity to work on impactful projects. At Lucid, I designed Quick Tools and Visual Activities, enhancing user interactions and the functionality of our products. My role also involved refining core features to improve the overall user experience.
               </p>
+              <p>
+              Looking ahead, I’m excited to explore interdisciplinary approaches to design, particularly in researching and creating hybrid digital-physical products. I’m passionate about pushing the boundaries of traditional design and creating innovative solutions that bridge the digital and physical worlds.
+              </p>
+              <p>
+              Outside of work, you can find me enjoying a visit to a local tea house, experimenting with new creative hobbies, or taking a walk with my dog.
+              </p>
+            </div>
+          </div>
+          <div className="logo-section">
+          <h2>I've had the joy of working with:</h2>  
+            <div className="logo-container">
+              <img src={uw} alt="University of Washington" />
+              <img src={brown} alt="Brown University" />
+              <img src={risd} alt="RISD" />
+              <img src={lucid} alt="Lucid Software" />
+              <img src={microsoft} alt="Microsoft" />
+              <img src={metro} alt="King County Metro" />
+              {/* Add more logos as needed */}
             </div>
           </div>
         </div>
