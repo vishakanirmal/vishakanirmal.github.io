b25b11b4b3ea62178fedbd6c320d3123186706c0
vishakanirmal <heyvishaka@gmail.com>
2025-03-29 17:36:41 -0400

    updates to home

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ import sketch2 from "/src/images/2.svg"
 import sketch3 from "/src/images/3.svg"
 
 const interactiveWords = [
-  { text: "intuitive experiences", color: "#E62A9E", sketch: sketch1, note: "In one of my first UX projects, I set up a fake museum for usability testing to see how people interacted with technology within the space.", top: "40%", left: "75%" },
-  { text: "strong collaborator", color: "#3300FF", sketch: sketch2, note: "My early collaborative projects involved building rube-goldberg marble runs in my friend’s garage.", top: "40%", left: "75%" },
+  { text: "intuitive interactions", color: "#E62A9E", sketch: sketch1, note: "In one of my first UX projects, I set up a fake museum for usability testing to see how people interacted with technology within the space.", top: "40%", left: "75%" },
+  { text: "strong collaborator", color: "#3300FF", sketch: sketch2, note: "By designing for the virtual canvas at Lucid Software, I've built a strong toolkit for collaboration, learning how teams across industries work together.", top: "40%", left: "75%" },
   { text: "ambitious technologist", color: "#FF7D19", sketch: sketch3, note: "The first time I explored designing in 3D was for a short film where I modeled and animated flying squirrels.", top: "40%", left: "75%" },
 ];
 
@@ -37,12 +37,12 @@ const IndexPage = () => {
               onMouseEnter={() => setHoveredWord(interactiveWords[0])}
               onMouseLeave={() => setHoveredWord(null)}
             >
-              intuitive experiences 
+              intuitive interactions 
               {hoveredWord === interactiveWords[0] && (
                 <span className="tooltip">{interactiveWords[0].note}</span>
               )}
             </span>
-            .
+            {" "}across digital and tangible experiences.
           </h1>
           <h1>
             She's also a{" "}
